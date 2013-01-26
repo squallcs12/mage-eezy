e7c19eb1413c560de7dd856512e82edbc253d90a
squallcs12 <antipro.vn@gmail.com>
2013-01-26 14:58:54 +0700

    auto add didyouknow css when run block

--- app/code/local/Eezy/Didyouknow/Block/Random.php
+++ app/code/local/Eezy/Didyouknow/Block/Random.php
@@ -1,8 +1,11 @@
 <?php
 class Eezy_Didyouknow_Block_Random extends Mage_Core_Block_Template {
 	protected function _prepareLayout() {
+        $head = $this->getLayout()->getBlock('head');
+        /* @var $head Mage_Page_Block_Html_Head */
+        $head->addCss('css/didyouknow.css');
+        
 		$this->setQuote ( Mage::helper ( 'didyouknow' )->getRandomQuote () );
-
 
 		$this->setTemplate($this->getTemplate());
 		
