889ca4138c9c571aec15a8c18960427292904c84
squallcs12 <antipro.vn@gmail.com>
2013-03-09 16:50:04 +0700

    add tag article list

--- app/code/local/Eezy/Note/Controller/Router.php
+++ app/code/local/Eezy/Note/Controller/Router.php
@@ -1,13 +1,13 @@
 <?php
 
-class Eezy_Note_Controller_Router extends Mage_Core_Controller_Varien_Router_Abstract{
-	/**
+class Eezy_Note_Controller_Router extends Mage_Core_Controller_Varien_Router_Abstract {
+
+    /**
      * Initialize Controller Router
      *
      * @param Varien_Event_Observer $observer
      */
-    public function initControllerRouters($observer)
-    {
+    public function initControllerRouters($observer) {
         /* @var $front Mage_Core_Controller_Varien_Front */
         $front = $observer->getEvent()->getFront();
 
@@ -20,31 +20,30 @@ class Eezy_Note_Controller_Router extends Mage_Core_Controller_Varien_Router_Abs
      * @param Zend_Controller_Request_Http $request
      * @return bool
      */
-    public function match(Zend_Controller_Request_Http $request)
-    {
+    public function match(Zend_Controller_Request_Http $request) {
         if (!Mage::isInstalled()) {
             Mage::app()->getFrontController()->getResponse()
-                ->setRedirect(Mage::getUrl('install'))
-                ->sendResponse();
+                    ->setRedirect(Mage::getUrl('install'))
+                    ->sendResponse();
             exit;
         }
 
         $identifier = trim($request->getPathInfo(), '/');
 
         $condition = new Varien_Object(array(
-            'identifier' => $identifier,
-            'continue'   => true
-        ));
+                    'identifier' => $identifier,
+                    'continue' => true
+                ));
         Mage::dispatchEvent('note_controller_router_match_before', array(
-            'router'    => $this,
+            'router' => $this,
             'condition' => $condition
         ));
         $identifier = $condition->getIdentifier();
 
         if ($condition->getRedirectUrl()) {
             Mage::app()->getFrontController()->getResponse()
-                ->setRedirect($condition->getRedirectUrl())
-                ->sendResponse();
+                    ->setRedirect($condition->getRedirectUrl())
+                    ->sendResponse();
             $request->setDispatched(true);
             return true;
         }
@@ -53,21 +52,35 @@ class Eezy_Note_Controller_Router extends Mage_Core_Controller_Varien_Router_Abs
             return false;
         }
 
-        $page   = Mage::getModel('note/article');
+        $page = Mage::getModel('note/article');
         $pageId = $page->checkIdentifier($identifier, Mage::app()->getStore()->getId());
-        if (!$pageId) {
-            return false;
+        if ($pageId) {
+
+            $request->setModuleName('note')
+                    ->setControllerName('article')
+                    ->setActionName('view')
+                    ->setParam('page_id', $pageId);
+            $request->setAlias(
+                    Mage_Core_Model_Url_Rewrite::REWRITE_REQUEST_PATH_ALIAS, $identifier
+            );
+            return true;
+        }
+        
+        $tag = Mage::getModel('note/tag');
+        $tagId = $tag->checkIdentifier($identifier, Mage::app()->getStore()->getId());
+        if ($tagId) {
+
+            $request->setModuleName('note')
+                    ->setControllerName('article')
+                    ->setActionName('index')
+                    ->setParam('filter', array('tag_id' => $tagId));
+            $request->setAlias(
+                    Mage_Core_Model_Url_Rewrite::REWRITE_REQUEST_PATH_ALIAS, $identifier
+            );
+            return true;
         }
 
-        $request->setModuleName('note')
-            ->setControllerName('article')
-            ->setActionName('view')
-            ->setParam('page_id', $pageId);
-        $request->setAlias(
-            Mage_Core_Model_Url_Rewrite::REWRITE_REQUEST_PATH_ALIAS,
-            $identifier
-        );
-
-        return true;
+        return false;
     }
+
 }
---
+++ app/code/local/Eezy/Note/controllers/TagController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Eezy_Note_TagController extends Mage_Core_Controller_Front_Action {
+
+    public function articleAction() {
+        $this->loadLayout()->renderLayout();
+    }
+
+}
