a967c2ec1576adcdce941609994b7785b1b61caa
squallcs12 <antipro.vn@gmail.com>
2013-03-16 09:13:58 +0700

    add search ref and best match for get mean

--- app/code/local/Eezy/Dictionary/Helper/Macmillan.php
+++ app/code/local/Eezy/Dictionary/Helper/Macmillan.php
@@ -4,29 +4,71 @@
  * 
  */
 class Eezy_Dictionary_Helper_Macmillan extends Mage_Core_Helper_Abstract {
+	
+	
+	
+	/**
+	 * Get definition from entry content
+	 * @param string $content
+	 * @return string
+	 */
+	protected function _getDefninition($content){
+		$index = strpos($content, '<DEFINITION>') + strlen('<DEFINITION>');
+		if($index === false){
+			return $this->getRefFromContent($content);
+		}
+        $index2 = strpos($content, '</DEFINITION>', $index);
+        $definition = substr($content, $index, $index2 - $index);
+        return strip_tags($definition);
+	}
+	
 
     public function getNewWord($w) {
         $word = Mage::getModel('dictionary/word');
         $word->setWord($w);
         $word->setKey('macmmillan');
-        $word->setMean($this->getMeanFromLive($w));
+        $word->setMean($this->getMeanFromEntry($w));
         if(!$word->getMean())
-            $word->setMean($this->getMeanFromLive(strtolower($w)));
-        $word->save();
+            $word->setMean($this->getMeanFromEntry(strtolower($w)));
+        //$word->save();
         return $word;
     }
 
-    public function getMeanFromLive($w) {
-        $response = $this->getEntry('american', $w);
-        $content = $response->entryContent;
-        $index = strpos($content, '<span class="DEFINITION">') + strlen('<span class="DEFINITION">');
-        $index2 = strpos($content, '</span>', $index);
-        $definition = substr($content, $index, $index2 - $index);
-        return $definition;
+	/**
+	 * Get Mean of a word from API
+	 * @param string $w word need to get mean
+	 * @return string
+	 */
+    public function getMeanFromEntry($w) {
+        $response = $this->getEntry($w);
+		if($response->errorCode == 'InvalidEntryId'){
+			return $this->getMeanFromBestMatch($w);
+		}
+        return $this->_getDefninition($response->entryContent);
     }
+	/**
+	 * 
+	 */
+	public function getMeanFromBestMatch($w){
+		$response = $this->getBestMatch($w);
+		return $this->_getDefninition($response->entryContent);
+	}
+	
+	/**
+	 * Get REF from a entry content
+	 * @param string $content
+	 * @return string
+	 */
+	public function getRefFromContent($content){
+		$index = strpos($content, '<GREF-TYPE>') + strlen('GREF-TYPE');
+		$index2 = strpos($content, '</GREF-TYPE>', $index);
+		$ref = substr($content, $index, $index2 - $index);
+		return strip_tags($ref);
+	}
 
     /**
-     * 
+     * Get an entry from API
+	 * 
      * @param string $dicCode
      * @param string $entryId
      * @param string $format
@@ -35,13 +77,18 @@ class Eezy_Dictionary_Helper_Macmillan extends Mage_Core_Helper_Abstract {
      * @param int $limit
      * @return type
      */
-    public function getEntry($dicCode, $entryId, $format = 'html', $page = 1, $start = 0, $limit = 25) {
+    public function getEntry($entryId, $dicCode = 'american', $format = 'xml', $page = 1, $start = 0, $limit = 25) {
         $url = "https://www.macmillandictionary.com/api/v1/dictionaries/$dicCode/entries/$entryId/?format=$format&page=$page&start=$start&limit=$limit";
         return $this->query($url);
     }
+	
+	public function getBestMatch($entryId, $dicCode = 'american', $format = 'xml', $page = 1, $start = 0, $limit = 25){
+		$url = "https://www.macmillandictionary.com/api/v1/dictionaries/$dicCode/search/first/?q=$entryId&format=$format&page=$page&start=$start&limit=$limit";
+        return $this->query($url);
+	}
 
     /**
-     * 
+     * Get API code
      * @return string
      */
     public function getCode() {
@@ -49,7 +96,8 @@ class Eezy_Dictionary_Helper_Macmillan extends Mage_Core_Helper_Abstract {
     }
 
     /**
-     * 
+     * Send query to API and get json_decoded content
+	 * 
      * @param string $url
      * @return object
      */
