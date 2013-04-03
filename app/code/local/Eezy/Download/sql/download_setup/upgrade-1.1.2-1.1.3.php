da1fd94f03f6a10f521040b4c7ec3479b206e2f0
Squall <squall@likipe.se>
2013-04-03 17:46:22 +0700

    add file model

---
+++ app/code/local/Eezy/Download/sql/download_setup/upgrade-1.1.2-1.1.3.php
@@ -0,0 +1,24 @@
+<?php
+
+/* @var $installer Mage_Customer_Model_Resource_Setup */
+$installer = $this;
+
+$table = $installer->getConnection()
+->newTable($installer->getTable('download/file'))
+->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+		'identity'  => true,
+		'unsigned'  => true,
+		'nullable'  => false,
+		'primary'   => true,
+), 'Id')
+->addColumn('file', Varien_Db_Ddl_Table::TYPE_TEXT, null, array(
+		'unsigned'  => true,
+		'nullable'  => false,
+		'default'   => '',
+), 'Filename including path')
+->addColumn('download_time', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+		'unsigned'  => true,
+		'nullable'  => false,
+		'default'   => '',
+), 'Download counter');
+$installer->getConnection()->createTable($table);
